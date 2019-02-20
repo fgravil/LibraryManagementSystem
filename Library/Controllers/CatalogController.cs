213d1148b650fa03064d5b99d3bffe1402d3eb31
Fred <fgravil18@gmail.com>
2019-02-19 21:04:23 -0500

    updates to catalog controller and checkout service

--- Library/Controllers/CatalogController.cs
+++ Library/Controllers/CatalogController.cs
@@ -1,20 +1,19 @@
 ﻿using Library.Models.Catalog;
 using LibraryData;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Library.Controllers
 {
     public class CatalogController : Controller
     {
         private ILibraryAsset _assets { get; set; }
+        private ICheckout _checkouts { get; set; }
 
-        public CatalogController(ILibraryAsset assets)
+        public CatalogController(ILibraryAsset assets, ICheckout checkouts)
         {
             _assets = assets;
+            _checkouts = checkouts;
         }
 
         public IActionResult Index()
@@ -32,6 +31,39 @@ namespace Library.Controllers
                 });
 
             var model = new AssetIndexModel() { Assets = listingResult };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _assets.GetById(id);
+
+            var currentHolds = _checkouts.GetCurrentHolds(id)
+                .Select(h => new AssetHoldModel
+                {
+                    HoldPlaced = _checkouts.GetCurrentHoldPlaced(h.Id).ToString("d"),
+                    PatronName = _checkouts.GetCurrentHoldPatronName(h.Id)
+                });
+
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Type = _assets.GetType(id),
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _assets.GetAuthorOrDirector(id),
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _assets.GetDeweyIndex(id),
+                ISBN = _assets.GetISBN(id),
+                CheckoutHistory = _checkouts.GetCheckoutHistory(id),
+                LatestCheckout = _checkouts.GetLatestCheckout(id),
+                PatronName = _checkouts.GetCurrentCheckoutPatron(id),
+                CurrentHolds = currentHolds
+            };
+
             return View(model);
         }
     }
